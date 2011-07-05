f8f7535314077cc6e2d3542cfe817dc5afcf0305
Nirmal Balasooriya <lbnnirmal@gmail.com>
2011-07-05 15:47:49 +0530

    Finished first cut of the User moel

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :users
+	
   map.contact '/contact', :controller => 'pages', :action => 'contact'
   map.about   '/about',   :controller => 'pages', :action => 'about'
   map.help    '/help',    :controller => 'pages', :action => 'help'
