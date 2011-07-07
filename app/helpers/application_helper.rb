6e94982afcbc4d1602a27f8189149f858796cf16
Nirmal Balasooriya <lbnnirmal@gmail.com>
2011-07-07 09:32:20 +0530

    mylast

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
 
   # Return a title on a per-page basis.
   def title
-    base_title = "Traning Allocation System"
+    base_title = "Ruby on Rails Tutorial Sample App"
     if @title.nil?
       base_title
     else
-      "#{base_title} | #{@title}"
+      "#{base_title} | #{h(@title)}"
     end
   end
 end
